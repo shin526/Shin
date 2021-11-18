ec94712a5a15c6ab5757eb6a87f4b9b7a8aee8e3
shin526 <shin99@dingtalk.com>
2021-11-18 17:28:04 +0800

    提交二分法

--- algorithm/search.py
+++ algorithm/search.py
@@ -17,9 +17,25 @@ def search(nums: list, target: int) -> int:
     return -1
 
 
+def search2(nums: list, target: int) -> int:
+    '''左闭右开'''
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        middle = (left + right) // 2
+        if nums[middle] < target:
+            left = middle + 1
+        elif nums[middle] > target:
+            right = middle
+        else:
+            return middle
+    return -1
+
+
 def main():
     nums = [5, 6, 9, 15, 18, 30, 45, 60, 78, 90]
     print(search(nums, 18))
+    print(search2(nums, 18))
 
 
 if __name__ == '__main__':
