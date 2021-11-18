377591dd1b1719127387af799ac9c8c67b0ed314
shin526 <shin99@dingtalk.com>
2021-11-18 17:59:24 +0800

    提交移除数组元素

---
+++ algorithm/removeElement.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# 原地移除所有数值等于 val 的元素，并返回移除后数组的新长度
+
+
+def removeElement(nums: list, val: int):
+    size = len(nums)
+    k = 0
+    for i in range(size):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+    return k
+
+
+def main():
+    nums = [0, 1, 2, 2, 3, 0, 4, 2]
+    print(removeElement(nums, 2))
+
+
+if __name__ == '__main__':
+    main()
