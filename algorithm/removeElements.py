96c00f908164cb7a9578ea2f361d2c6b73f64cae
shin526 <shin99@dingtalk.com>
2021-11-23 09:30:38 +0800

    修改移除元素

--- algorithm/removeElements.py
+++ algorithm/removeElements.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python
 # 删除链表中等于给定值 val 的所有节点
 
-
-class ListNode:
-    __slots__ = ('val', 'next')
-
-    def __init__(self, val) -> None:
-        self.val = val
-        self.next = None
+from LinkedList import ListNode, LinkedList
 
 
 def removeElements(head: ListNode, val):
@@ -22,33 +16,13 @@ def removeElements(head: ListNode, val):
     return head
 
 
-def createList():
-    A = ListNode(1)
-    B = ListNode(1)
-    C = ListNode(6)
-    D = ListNode(3)
-    E = ListNode(1)
-    F = ListNode(5)
-    G = ListNode(6)
-    A.next = B
-    B.next = C
-    C.next = D
-    D.next = E
-    E.next = F
-    F.next = G
-    head = A
-    return head
-
-
 def main():
-    head = createList()
-    head = removeElements(head, 1)
-    res = []
-    cur = head
-    while cur:
-        res.append(cur.val)
-        cur = cur.next
-    print(res)
+    items = [1, 3, 6, 5, 6, 7, 4]
+    l1 = LinkedList()
+    for item in items:
+        l1.addTail(item)
+    l1.head = removeElements(l1.head, 6)
+    l1.display()
 
 
 if __name__ == '__main__':
