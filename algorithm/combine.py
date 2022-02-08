d8b48002ace53126e78d014065536159d4c8d885
shin526 <shin99@dingtalk.com>
2022-02-08 20:55:30 +0800

    提交组合

---
+++ algorithm/combine.py
@@ -0,0 +1,28 @@
+# usr/bin/python
+# 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合
+
+
+def combine(n: int, k: int):
+    res = []
+    path = []
+
+    def backtrack(n, k, start):
+        if len(path) == k:
+            res.append(path[:])
+
+            return
+        for i in range(start, n + 1):
+            path.append(i)
+            backtrack(n, k, i + 1)
+            path.pop()
+
+    backtrack(n, k, 1)
+    return res
+
+
+def main():
+    print(combine(4, 2))
+
+
+if __name__ == '__main__':
+    main()
